51b87ed97d55b613a19fa1199c961a3150b0556e
Miguel.Essien <156939629+Miel230@users.noreply.github.com>
2024-10-16 22:37:23 -0300

    adicionando as novas funções

--- lib/pages/login_page.dart
+++ lib/pages/login_page.dart
@@ -1,6 +1,9 @@
 import 'package:flutter/material.dart';
 
 class LoginPage extends StatelessWidget {
+  final _tLogin = TextEditingController();
+  final _tSenha = TextEditingController();
+
   @override
   Widget build(BuildContext context) {
     return Scaffold(
@@ -16,37 +19,23 @@ class LoginPage extends StatelessWidget {
       padding: EdgeInsets.all(16),
       child: ListView(
         children: [
-          _text("Login", "Digite o login"),
+          _text("Login", "Digite o login", controller: _tLogin),
           SizedBox(
             height: 10,
           ),
-          _text("Senha", "Digite a senha"),
+          _text("Senha", "Digite a senha", controller: _tSenha),
           SizedBox(
             height: 20,
           ),
-          _button("Login"),
+          _button("Login", _onClickLogin),
         ],
       ),
     );
   }
 
-  _button(String text) {
-    return Container(
-      child: ElevatedButton(
-        child: Text(
-          text,
-          style: TextStyle(color: Colors.white, fontSize: 25),
-        ),
-        onPressed: () {},
-        style: ElevatedButton.styleFrom(
-          backgroundColor: Colors.blue, // Background color
-        ),
-      ),
-    );
-  }
-
-  _text(String label, String hint) {
+  _text(String label, String hint, {TextEditingController? controller}) {
     return TextFormField(
+      controller: controller,
       style: TextStyle(fontSize: 25, color: Colors.blue),
       decoration: InputDecoration(
           labelText: label,
@@ -54,5 +43,27 @@ class LoginPage extends StatelessWidget {
           hintText: hint,
           hintStyle: TextStyle(fontSize: 16)),
     );
+  }
+
+  _button(String text, Function onPressed) {
+    return Container(
+      child: ElevatedButton(
+        child: Text(
+          text,
+          style: TextStyle(color: Colors.white, fontSize: 25),
+        ),
+        onPressed: () => onPressed(),
+        style: ElevatedButton.styleFrom(
+          backgroundColor: Colors.blue, // Background color
+        ),
+      ),
+    );
+  }
+
+  void _onClickLogin() {
+    String login = _tLogin.text;
+    String senha = _tSenha.text;
+
+    print("Login: $login, Senha: $senha");
   }
 }
